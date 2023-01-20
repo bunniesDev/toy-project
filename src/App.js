08b67a19ece29d1772f64f0622c644b06e3cfe8a
김지아 <gimjia@gimjiaui-MacBookAir.local>
2023-01-21 07:32:25 +0900

    testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         여기다 글씨바꿔라
         </p>
         <a
           className="App-link"
